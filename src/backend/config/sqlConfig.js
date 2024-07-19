68cced4385e9f756be186b5b6cad828fe4823b25
Abhilash Sarangi <132396257+dev1abhi@users.noreply.github.com>
2024-07-19 11:24:48 +0530

    Update sqlConfig.js

--- src/backend/config/sqlConfig.js
+++ src/backend/config/sqlConfig.js
@@ -5,23 +5,6 @@ const { encrypt } = require("web-push");
 
 // config/sqlConfig.js
 module.exports = {
-  user:  'itoms',
-  password: 'nalco@123',
-  database: 'itomsdb' ,
-  server: '10.60.235.111',
-  driver:'msnodesqlv8',
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 720000
-  },
-  requestTimeout: 720000,
-  options: {
-    trustedConnection: true,
-    encrypt: true, // for azure
-    trustServerCertificate: true, // change to true for local dev / self-signed certs
-    idleTimeoutMillis: 720000,
-  },
 
 };
 
